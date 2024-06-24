f37512c6dbc1e018ee0f0d4348771162e7824a96
Scutun <130495208+Scutun@users.noreply.github.com>
2024-06-24 16:42:56 +0300

    add wearhouse

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const wearhouseRoute = require('./routes/wearhouse.routes')
+
+
 
 
 app.use(express.json())
 const PORT = process.env.port || 3000
 app.use(cors())
-//app.use('/api', )
+app.use('/api', wearhouseRoute)
+
+
+
 
 app.listen(PORT, () => {
     console.log('Server work')
---
+++ routes/wearhouse.routes.js
@@ -0,0 +1,9 @@
+const Router = require('express')
+const router = new Router()
+const wearhouseController = require('../controllers/wearhouse.controller')
+
+router.post('/wearhouse/create', wearhouseController.createWearhouse)
+router.get('/wearhouse/find/:id', wearhouseController.getWearhouse)
+router.delete('/wearhouse/delete/:id', wearhouseController.deleteWearhouse)
+
+module.exports = router
